61c8124f6bc117fd9493f2adb8fa7ee5b93f505b
Fsesnick <fernandasesnick@gmail.com>
2021-02-13 20:59:30 +0000

    using Date()

--- pet.js
+++ pet.js
@@ -1,7 +1,26 @@
 class Pet {
-  constructor(animal, age, breed){
+  constructor(animal, age, breed,sound){
     this.animal = animal;
     this.age    = age;
-    this breed  = breed;
+    this.breed  = breed;
+    this.sound  = sound;
   }
+  
+  get activity(){
+    const today = new Date();
+    const hours = today.getHours();
 }
+  
+  speak(){
+  console.log(this.sound);
+  }
+  
+}
+/*
+const ernie = new Pet('dog',1,'pug', 'yip yip');
+const vera = new Pet('dog', 8, 'border collie', 'woof woof');
+
+console.log(ernie);
+
+vera.speak();
+*/
